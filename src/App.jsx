0e11c283c6c5ab33a2940504a951f26c248c435b
Long Bora <92431773+longbora97@users.noreply.github.com>
2025-01-20 17:45:23 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-const myOS = require("os")
 
 function App() {
   const [name, setName] = useState("");
@@ -26,7 +25,6 @@ function App() {
     <div className="container mx-auto p-5 font-thin">
       <div className="p-5 w-full">
         <h1 className="text-center text-6xl font-thin">Insert Form</h1>
-{/*         <h1 className="text-center font-thin">{myOS.userInfo()}</h1> */}
       </div>
       <div className="w-full md:flex">
         <div className="w-full md:w-1/4">
